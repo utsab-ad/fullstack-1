8c43b9fa52a8307f8a63d86db00e29e3686397f7
utsab-ad <utsabadhikari075@gmail.com>
2025-04-07 21:26:37 +0545

    prepared

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(
@@ -75,6 +76,6 @@ app.get("/users", verifyUser, (req, res) => {
 });
 
 
-app.listen(3001,'0.0.0.0', () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`http://localhost:3001`);
 });
